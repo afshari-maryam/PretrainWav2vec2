3fa37c741d8623a64774579afcbef5643820a2ac
afshari-maryam <afshari1431@gmail.com>
2023-01-02 18:08:07 +0200

    add dataset module version check

--- check_transformers_library.py
+++ check_transformers_library.py
@@ -1,5 +1,7 @@
 # Import the transformers library
 import transformers
+import datasets
 
 # Print the version of the transformers library
 print(transformers.__version__)
+print(datasets.__version__)
