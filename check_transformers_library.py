c29e36f63c2e41b66f00285fc9304905bca34ce6
afshari-maryam <afshari1431@gmail.com>
2023-01-02 18:25:42 +0200

    add pydub and gradio module to check versions

--- check_transformers_library.py
+++ check_transformers_library.py
@@ -1,7 +1,11 @@
 # Import the transformers library
 import transformers
 import datasets
+import gradio as gr
+import pydub
 
 # Print the version of the transformers library
 print(transformers.__version__)
 print(datasets.__version__)
+print(gr.__version__)
+print(pydub.__version__)
