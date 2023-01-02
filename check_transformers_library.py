c4ec84cd493e14f2c1180d30819c322729de48ec
afshari-maryam <afshari1431@gmail.com>
2023-01-02 18:45:41 +0200

    Add first part to my code and check the device

--- check_transformers_library.py
+++ check_transformers_library.py
@@ -11,3 +11,17 @@ print(gr.__version__)
 #print(pydub.__version__)
 print("In the name of GOD")
 print("Date: 2-Janyuary-2023")
+
+import os
+import gradio as gr
+import torch
+import pydub
+import torchaudio
+from torchaudio.sox_effects import apply_effects_tensor
+import numpy as np
+from transformers import AutoFeatureExtractor, AutoModelForAudioXVector
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#print(f"I'm learning {language} from {school}.")
+print(f"device = {device} .")
