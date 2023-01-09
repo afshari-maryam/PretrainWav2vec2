d5aae59eb04afe25aa6fc73ec8da27d882bb2861
afshari-maryam <afshari1431@gmail.com>
2023-01-09 07:41:46 +0200

    add writing in file

--- WavLM_Voxceleb_Evaluation_part1.py
+++ WavLM_Voxceleb_Evaluation_part1.py
@@ -10,7 +10,10 @@ from torchaudio.sox_effects import apply_effects_tensor
 import numpy as np
 from transformers import AutoFeatureExtractor, AutoModelForAudioXVector
 import itertools
-import numpy as np
+import numpy as np  
+import pandas as pd  
+  
+     
 #from sklearn.metrics import roc_curve, eer
 
 # Print the version of the transformers library
@@ -95,7 +98,20 @@ def compute_scores(spkr_list1,spkr_list2):
     file_path2 = os.path.join(root, path2)
     # Add the similarity to the list score
     scores.append(similarity_fn(file_path1,file_path2))
+  print("comput scores function ended ....................................")
   return scores
+
+
+def write_score_in_csv_file(input_score):
+    # list score 
+    scr = input_score
+    # dictionary of lists  
+    dict = {'score': scr}  
+       
+    df = pd.DataFrame(dict) 
+    
+    # saving the dataframe 
+    df.to_csv('my_score_cfg.csv') 
 
 #------------------this Part add for voxceleb reading------------------
 #import os
@@ -130,8 +146,12 @@ similarity_fn(file_path1,file_path2)
 #------------------call compute score functions ------------------------------------------------
 
 my_scores =compute_scores(speaker1,speaker2)
-
 print(f"my_scores[0] = {my_scores[0]}")
+
+#save score in a csv file
+print("write scores in a csv file using pandas")
+write_score_in_csv_file(my_scores)
+
 
 ## Convert the labels to a numpy array
 #labels = np.array([label1 == label2 for _, label1 in dataset for _, label2 in dataset])
